ea126e84f5589593c433f76bd1ecb6ffbdce69b5
Gasnis <gastonsaravia112@gmail.com>
2023-02-22 07:35:25 +0100

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import Login from './components/Login/Login.jsx';
 import SignUp from './components/SignUp/SignUp';
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://grupo13-pf-production.up.railway.app/";
-axios.defaults.baseURL = "http://localhost:3001/"
+axios.defaults.baseURL = "https://e-commerce-production-f31a.up.railway.app/";
+// axios.defaults.baseURL = "http://localhost:3001/"
 
 
 function App() {
